2382965833292454b7d73949829fc1c17008c9a1
AlishaSadiqdeveloper <alishasadiq21@gmail.com>
2025-04-26 17:36:16 +0500

    Add files via upload

---
+++ circle.js
@@ -0,0 +1,14 @@
+let r=Number(prompt("enter radius of a circle"));
+console.log("radius Of a Circle");
+
+let d=2*r;
+console.log(d);
+const pi=3.14159;
+let C=pi*d;
+console.log(C);
+let A=pi*r**2;
+console.log(A);
+
+
+
+
